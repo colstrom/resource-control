c9fd66367f03d4b3c95eb4471c9aa106578a6779
Chris Olstrom <chris@olstrom.com>
2015-05-15 15:59:30 -0700

    Rough idea of how this could be used. Unless you have a better way, in which case do that instead.

---
+++ acl.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'hiredis'
+require 'redis'
+
+require_relative 'lib/user'
+require_relative 'lib/group'
+require_relative 'lib/resource'
+
+def redis
+  @redis ||= Redis.new
+end
+
+user, resource, value = ARGV.pop 3
+
+Group.setup unless  redis.exists 'acl.groups'
+User.setup unless redis.exists 'acl.users'
+
+Group.add_to 'dev', 'ben'
+Group.add_to 'ops', 'chris'
+
+Resource.grant 'onlyops', 'ops'
+
+if User.exists?(user) and Resource.allowed?(resource, user)
+  Resource.change resource, value
+  puts Resource.read resource
+else
+  puts "#{resource} is #{Resource.read resource}"
+end
